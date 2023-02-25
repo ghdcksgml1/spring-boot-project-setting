8d999bd6dc104ca8d6ee207805c6ddc4e5952287
ghdcksgml1 <ghdcksgml2@naver.com>
2023-02-25 18:48:53 +0900

    add: multi module configuration

---
+++ build.gradle.kts
@@ -0,0 +1,67 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.0.3"
+	id("io.spring.dependency-management") version "1.1.0"
+	kotlin("jvm") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"
+}
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+allprojects {
+	group = "com.proseed"
+	version = "0.0.1-SNAPSHOT"
+
+	repositories {
+		mavenCentral()
+	}
+}
+
+subprojects {
+
+	apply(plugin = "java")
+
+	apply(plugin = "io.spring.dependency-management")
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+
+	apply(plugin = "kotlin")
+	apply(plugin = "kotlin-spring") //all-open\
+
+	dependencies {
+		implementation("org.springframework.boot:spring-boot-starter-web")
+		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+		developmentOnly("org.springframework.boot:spring-boot-devtools")
+		testImplementation("org.springframework.boot:spring-boot-starter-test")
+	}
+
+	dependencyManagement {
+		imports {
+			mavenBom(org.springframework.boot.gradle.plugin.SpringBootPlugin.BOM_COORDINATES)
+		}
+
+		dependencies {
+			dependency("net.logstash.logback:logstash-logback-encoder:6.6")
+		}
+	}
+
+	tasks.withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs = listOf("-Xjsr305=strict")
+			jvmTarget = "17"
+		}
+	}
+
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
+
+	configurations {
+		compileOnly {
+			extendsFrom(configurations.annotationProcessor.get())
+		}
+	}
+}
